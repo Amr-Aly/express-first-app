c875082367873265d69d73f5f05f17f057f7df01
Amr Ali <amr.ali@linkdev.com>
2020-01-22 12:41:17 +0200

    changes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = express();
 
 var envVar = process.env.EnvVar;
 
+var port = process.env.PORT || 4000;
+
 app.get('/', function (req, res) {
     res.send('Hello, world!');
 });
@@ -13,6 +15,6 @@ app.get('/env', function (req, res) {
     res.send(envVar);
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log('app listening on port 3000...');
 });
